d139b5c49ebd7271df05a215fd07d326a5edc023
Vineeth Pillai <vineethrp@gmail.com>
2017-11-24 17:55:08 +0000

    Minor code cleanup in largestcomponent

--- src/largestcomponent.c
+++ src/largestcomponent.c
@@ -35,13 +35,11 @@ find_connected_component(bool *threshold_data,
 connected_component_t
 largest_component(bool *threshold_data, int x1, int y1, int x2, int y2, int w, int h)
 {
-  int i, j;
   connected_component_t largest_component = { 0 };
 
-  for (i = x1; i < x2; i++) {
-    bool *thresh_row = threshold_data + (i * w);
-    for (j = y1; j < y2; j++) {
-      if (*(thresh_row + j) == 1) {
+  for (int i = x1; i < x2; i++) {
+    for (int j = y1; j < y2; j++) {
+      if (*(threshold_data + (i * w) + j)) {
         connected_component_t c = { 0 };
         find_connected_component(threshold_data, i, j, w, h, &c);
         LOG_DEBUG("Component: %d %d %d", c.total_x, c.total_y, c.count);
